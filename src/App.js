b34cf885cdbdef117e789ba079d9b3523ae0a55e
Mit Shah <mits@mangospring.com>
2022-01-06 14:53:13 +0530

    Nah! Deleted it again

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
       <h1>Hello World!! This is Mit</h1>
       <p>This is something</p>
       <p>This is something else.</p>
-      <p>This is something else else.</p>
     </div>
   );
 }
